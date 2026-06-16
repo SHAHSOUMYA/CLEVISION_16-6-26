79c7c0037451c83e01181830e64c7de2d156c2b9
SHAH SOUMYA <shahsoumya765@gmail.com>
2026-06-16 12:46:24 +0530

    Add files via upload

---
+++ groupstudent.js
@@ -0,0 +1,10 @@
+const students = [
+  { name: 'Aarav', marks: 78, passed: true, subject: 'Math' },
+  { name: 'Diya', marks: 42, passed: false, subject: 'Science' },
+  { name: 'Kabir', marks: 88, passed: true, subject: 'Math' },
+  { name: 'Meera', marks: 65, passed: true, subject: 'English' },
+  { name: 'Rohan', marks: 38, passed: false, subject: 'Science' },
+  { name: 'Isha', marks: 92, passed: true, subject: 'Math' },
+  { name: 'Vivaan', marks: 75, passed: true, subject: 'English' },
+  { name: 'Anaya', marks: 70, passed: true, subject: 'Science' }
+]
